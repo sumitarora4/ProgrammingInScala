94fa0fc6ed9fc27f478c54db7136b55a9f7dee70
sumit arora <sumitarora4@gmail.com>
2021-08-05 20:59:17 +0530

    chapter6

--- src/main/scala/Chapter6/Chapter6.sc
+++ src/main/scala/Chapter6/Chapter6.sc
@@ -1,15 +1,20 @@
+import Chapter6.Rational
 //import Chapter6.Rational
-//import Chapter6.Rational
 
+//
+//class Rational(n: Int, d: Int) {
+//  //  println("Created"+ n +"/"+ d)
+//
+//  override def toString = s"$n/$d"
+//}
 
-class Rational(n: Int, d: Int) {
-  //  println("Created"+ n +"/"+ d)
+val newR = new Rational(5,0)
 
-  override def toString = s"$n/$d"
-}
+val oneh = new Rational(1,2)
+val twoth = new Rational(2,3)
 
-val newR = new Rational(1,2)
-println(newR)
+oneh add(twoth)
+//println(newR)
 //println("onehalf="+oneHalf)
 
-println("sumit")
+//println("sumit")
--- src/main/scala/Chapter6/Rational.scala
+++ src/main/scala/Chapter6/Rational.scala
@@ -1,10 +1,81 @@
 package Chapter6
 
 class Rational(n: Int, d: Int) {
-//  println("Created"+ n +"/"+ d)
 
-  override def toString = s"$n/$d"
+//   for rational number denominator should not be zero
+//  other wise this will give result infinite
+  require(d != 0)
+
+  private  val g = gcd(n.abs, d.abs)
+  val number: Int = n/g
+  val denom: Int = d/g
+
+//    println("Created"+ n +"/"+ d)
+  override def toString = s"$number/$denom"
+
+//   adding two rational numbers
+  def add(that: Rational): Rational = {
+    new Rational(number * that.denom + that.number * denom, denom * that.denom)
+
+  }
+
+//  tests whether the given Rational is smaller than a parameter
+  def lessThan(that: Rational) =
+    this.number * that.denom < that.number * this.denom
+
+// find greater of the given rational number and an argument
+  def max(that: Rational) =
+    if(this.lessThan(that)) that else this
+
+//  auxiliary constructors uses: If denominator is 1 then rational
+//  number is a normal number
+  def this(n : Int) = this(n,1)
+
+//  for normalize rational number
+  private def gcd(a: Int, b: Int): Int =
+    if(b ==0) a else gcd(b, a % b)
+
+// define operator add operator
+//  add operator for adding two rational number
+  def + (that: Rational) : Rational =
+    new Rational(number * that.denom + that.number * denom, denom * that.denom)
+
+//  Method Overloading is uses: If rational number is multiply by any integer or
+//  added by any integer or number
+  def + (i : Int ): Rational =
+    new Rational(number + i * denom , denom)
+
+//  define operator subtract operator
+//  subtract operator for adding two rational number
+  def - (that: Rational) : Rational =
+    new Rational(number * that.denom - that.number * denom, denom * that.denom)
+
+//  Method Overloading is uses: If rational number is subtract by any integer
+  def - (i : Int ): Rational =
+    new Rational(number - i * denom , denom)
+
+//  multiply operator for multiply two rational number
+  def * (that: Rational): Rational =
+    new Rational(number * that.number, denom * that.denom)
+
+  //  Method Overloading is uses: If rational number is multiply by any integer
+  def * (i : Int ): Rational =
+    new Rational(number * i  , denom)
+
+  //  divide operator for division of two rational number
+  def / (that: Rational): Rational =
+    new Rational(number * that.denom, denom * that.number)
+
+  //  Method Overloading is uses: If rational number is divide by any integer
+  def / (i : Int ): Rational =
+    new Rational(number  , denom * i)
+
+  implicit def intToRational(x : Int) = new Rational(x)
+
 }
+
+
+
 
 //object Rational {
 //
